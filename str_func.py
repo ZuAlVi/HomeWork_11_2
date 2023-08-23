e800eded01559ff1ef06985574f4929a610abe65
Alexandr Zubarev <zualvi@yandex.ru>
2023-08-23 20:14:41 +0300

    add docstring in func funtion and add title_func funtion

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def func(value):
+    """The function returns the string in uppercase"""
     return value.upper()
+
+
+def title_func(value):
+    """The function returns a string with capitalized words"""
+    temp_list = value.split()
+    total_string = ''
+    for item in temp_list:
+        total_string += ' ' + ''.join(item.title())
+    return total_string.strip()
